2f5781910109b4d4c1478e90ceb79b48fd5eb07e
lickx <sinesurfer@gmail.com>
2022-12-06 05:46:38 +0100

    Fix some issues & identation

--- stats.php
+++ stats.php
@@ -1,6 +1,5 @@
 <?php
 
-$website = "http://yourgridurl.xxx";
 $loginscreen = "path_to_your_login_screen";
 $robustURL   = "yourgridurl"; //FQDN or IP to your grid/robust server
 $robustPORT = "8002"; //port for your robust
@@ -12,21 +11,18 @@ $user = "username";
 $pass = "pass";
 $dbname = "dbname";
 
-
 // Online / Offline with socket
 $socket = @fsockopen($robustURL, $robustPORT, $errno, $errstr, 1);
 if (is_resource($socket))
 {
-$gstatus = "ONLINE";
-$color = "green";
+    $gstatus = "ONLINE";
+    $color = "green";
+    @fclose($socket);
 }
 else {
-$gstatus = "OFFLINE";
-$color = "red";
+    $gstatus = "OFFLINE";
+    $color = "red";
 }
-@fclose($socket);
-
-
 
 $mysqli = new mysqli($host,$user,$pass,$dbname);
 $presenceuseraccount = 0;
@@ -34,81 +30,83 @@ $presenceuseraccount = 0;
 $monthago = time() - 2592000; 
 
 if ($hguser = $mysqli->query("SELECT UserID, Login FROM GridUser WHERE UserID LIKE '%htt%' AND Login < 'time() - 2592000'")) 
-		{
-			$preshguser= $hguser->num_rows;
-		}
-	
-
+{
+    $preshguser= $hguser->num_rows;
+}
 
 $nowonlinescounter = 0;
 if ($preso = $mysqli->query("SELECT UserID FROM Presence")) {
-	$nowonlinescounter = $preso->num_rows;
+    $nowonlinescounter = $preso->num_rows;
 }
 $pastmonth = 0;
-if ($tpres = $mysqli->query("SELECT DISTINCT * FROM GridUser WHERE Logout < '".$monthago."'")) {
-	$pastmonth = $tpres->num_rows;
+if ($tpres = $mysqli->query("SELECT DISTINCT * FROM GridUser WHERE UserID NOT LIKE '%http%' AND Login < 'time() - 2592000'")) {
+    $pastmonth = $tpres->num_rows;
 }
 $totalaccounts = 0;
 if ($useraccounts = $mysqli->query("SELECT * FROM UserAccounts")) {
-	$totalaccounts = $useraccounts->num_rows;
+    $totalaccounts = $useraccounts->num_rows;
 }
 $totalregions = 0;
 $totalvarregions = 0;
 $totalsingleregions = 0;
 $totalsize = 0;
 if($regiondb = $mysqli->query("SELECT * FROM regions")) {
-	while ($regions = $regiondb->fetch_array()) {
-		++$totalregions;
-		if ($regions['sizeX'] == 256) {
-			++$totalsingleregions;
-		}else{
-			++$totalvarregions;
-		}
-		$rsize = $regions['sizeX'] * $regions['sizeY'];
-		$totalsize += $rsize / 1000;
-	}
+    while ($regions = $regiondb->fetch_array()) {
+        ++$totalregions;
+        if ($regions['sizeX'] == 256) {
+            ++$totalsingleregions;
+        } else {
+            ++$totalvarregions;
+        }
+        $rsize = $regions['sizeX'] * $regions['sizeY'];
+        $totalsize += $rsize / 1000;
+    }
 }
-$arr = ['GridStatus' => '<b><font color="'.$color.'">'.$gstatus.'</b></font>',
-	'Online_Now' => number_format($nowonlinescounter),
-	'HG_Visitors_Last_30_Days' => number_format($preshguser),
-	'Local_Users_Last_30_Days' => number_format($pastmonth),
-	'Total_Active_Last_30_Days' => number_format($pastmonth + $preshguser),
-	'Registered_Users' => number_format($totalaccounts),
-	'Regions' => number_format($totalregions),
-	'Var_Regions' => number_format($totalvarregions),
-	'Single_Regions' => number_format($totalsingleregions),
-	'Total_LandSize(km2)' => number_format($totalsize),
-	'Login_URL' => $loginuri,
-	'Website' => '<i><a href='.$website.'>'.$website.'</a></i>',
-	'Login_Screen' => '<i><a href='.$loginscreen.'>'.$loginscreen.'</a></i>'];
-	
-if ($_GET['format'] == "json") {
-	header('Content-type: application/json');
-	echo json_encode($arr);
-}else if ($_GET['format'] == "xml") {
-	function array2xml($array, $wrap='Stats', $upper=true) {
-	    $xml = '';
-	    if ($wrap != null) {
-	        $xml .= "<$wrap>\n";
-	    }
-	    foreach ($array as $key=>$value) {
-	        if ($upper == true) {
-	            $key = strtoupper($key);
-	        }
-	        $xml .= "<$key>" . htmlspecialchars(trim($value)) . "</$key>";
-	    }
-	    if ($wrap != null) {
-	        $xml .= "\n</$wrap>\n";
-	    }
-	    return $xml;
-	}
-	header('Content-type: text/xml');
-	print array2xml($arr);
-}else{
-	foreach($arr as $k => $v) {
-		echo '<B>'.$k.': </B>'.$v.'<br>';
-	}
 
-	}
+$arr = ['GridStatus' => '<b><font color="'.$color.'">'.$gstatus.'</b></font>',
+    'Online_Now' => number_format($nowonlinescounter),
+    'HG_Visitors_Last_30_Days' => number_format($preshguser),
+    'Local_Users_Last_30_Days' => number_format($pastmonth),
+    'Total_Active_Last_30_Days' => number_format($pastmonth + $preshguser),
+    'Registered_Users' => number_format($totalaccounts),
+    'Regions' => number_format($totalregions),
+    'Var_Regions' => number_format($totalvarregions),
+    'Single_Regions' => number_format($totalsingleregions),
+    'Total_LandSize(km2)' => number_format($totalsize),
+    'Login_URL' => $loginuri,
+    'Website' => '<i><a href='.$website.'>'.$website.'</a></i>',
+    'Login_Screen' => '<i><a href='.$loginscreen.'>'.$loginscreen.'</a></i>'];
+
+if (isset($_GET['format']) && $_GET['format'] == "json") {
+    header('Content-type: application/json');
+    echo json_encode($arr);
+} else if (isset($_GET['format']) && $_GET['format'] == "xml") {
+    function array2xml($array, $wrap='Stats', $upper=true) {
+        $xml = '';
+        if ($wrap != null) {
+            $xml .= "<$wrap>\n";
+        }
+        foreach ($array as $key=>$value) {
+            if ($upper == true) {
+                $key = strtoupper($key);
+            }
+            $xml .= "<$key>" . htmlspecialchars(trim($value)) . "</$key>";
+        }
+        if ($wrap != null) {
+            $xml .= "\n</$wrap>\n";
+        }
+        return $xml;
+    }
+    header('Content-type: text/xml');
+    print array2xml($arr);
+} else {
+    echo "<!DOCTYPE html>\n<html>\n<head>\n<title>Isthmus - Statistics</title>\n" .
+         "<link rel=\"stylesheet\" href=\"style.css\">\n</head>\n<body>\n";
+    foreach($arr as $k => $v) {
+        echo '<strong>'.$k.': </strong>'.$v."<br>\n";
+    }
+    echo "</body>\n</html>";
+}
 $mysqli->close();
 ?>
+
